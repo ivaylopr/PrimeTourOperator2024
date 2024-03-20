d40b197c089069bcd748dba8d1c2c1695be53778
Ивайло Пръмов <115698520+ivaylopr@users.noreply.github.com>
2024-03-20 11:02:37 +0200

    adding password requirements

--- PrimeTourOperator/Extensions/ServiceConnectionExtension.cs
+++ PrimeTourOperator/Extensions/ServiceConnectionExtension.cs
@@ -5,25 +5,32 @@ using PrimeTourOperator.Infrastructure.Data;
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceConnectionExtension
-	{
-		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
-		{
-			return services;
-		}
-		public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
-		{
-			var connectionString = config.GetConnectionString("DefaultConnection");
-			services.AddDbContext<ApplicationDbContext>(options =>
-				options.UseSqlServer(connectionString));
-			services.AddDatabaseDeveloperPageExceptionFilter();
-			return services;
-		}
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            return services;
+        }
 
-		public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
-		{
-			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-			.AddEntityFrameworkStores<ApplicationDbContext>();
-			return services;
-		}
-	}
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+                options.Password.RequireDigit = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+            return services;
+        }
+    }
 }
